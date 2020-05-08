63413d774d5a5fd676f39d9c811a698a32805565
Mariangelica Useche <mus@Mariangelicas-MacBook-Pro.local>
2020-05-07 23:09:51 -0500

    Add logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -62,7 +62,13 @@ export default function Header(props) {
     [classes.absolute]: absolute,
     [classes.fixed]: fixed
   });
-  const brandComponent = <Button className={classes.title}>{brand}</Button>;
+  const brandComponent = (
+    <Button className={classes.title}>
+      <img src={logo} width={50} />
+      {brand}
+    </Button>
+  );
+
   return (
     <AppBar className={appBarClasses}>
       <Toolbar className={classes.container}>
